fef6a6c0513b95453f959fa11156b9f227c2cc60
Duy Phan <duyphan@lecle.co.kr>
2021-01-28 11:54:53 +0700

    feat: add drag and drop for note

--- packages/extension/scripts/inject.js
+++ packages/extension/scripts/inject.js
@@ -7,6 +7,9 @@ document.body.appendChild(layer);
 
 layer.onmousedown = (evt) => {
   const { pageX, pageY } = evt;
+  if (evt.target.className && evt.target.className.startsWith("site-note "))
+    return;
+
   const note = createNote(pageX, pageY);
   const { top, left } = note.style;
   notes.push({ note, x: left, y: top });
@@ -37,7 +40,44 @@ function createNote(x, y) {
     `top:${y - scrollY - 10}px;left:${x - scrollX - 10}px`
   );
 
+  addDnD(note);
   layer.appendChild(note);
 
   return note;
+}
+
+function addDnD(ref) {
+  ref.onmousedown = function (event) {
+    if (event.target.nodeName === "INPUT") return;
+    let shiftX = event.clientX - ref.getBoundingClientRect().left;
+    let shiftY = event.clientY - ref.getBoundingClientRect().top;
+
+    moveAt(event.pageX, event.pageY);
+
+    // moves the div at (pageX, pageY) coordinates
+    // taking initial shifts into account
+    function moveAt(pageX, pageY) {
+      const { scrollX, scrollY } = window;
+
+      ref.style.left = pageX - shiftX - scrollX + "px";
+      ref.style.top = pageY - shiftY - scrollY + "px";
+    }
+
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+
+    // move the div on mousemove
+    document.addEventListener("mousemove", onMouseMove);
+
+    // drop the div, remove unneeded handlers
+    ref.onmouseup = function () {
+      document.removeEventListener("mousemove", onMouseMove);
+      ref.onmouseup = null;
+    };
+  };
+
+  ref.ondragstart = function () {
+    return false;
+  };
 }
