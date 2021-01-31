b35e3121109b60a638bd9cbee2a76db0e8d51484
Duy Phan <duyphan@lecle.co.kr>
2021-01-31 10:48:41 +0700

    feat: create dynamodb table configuration

--- packages/back-end/serverless.ts
+++ packages/back-end/serverless.ts
@@ -1,39 +1,55 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
 const serverlessConfiguration: AWS = {
-  service: 'back-end',
-  frameworkVersion: '2',
+  service: "site-note-service",
+  frameworkVersion: "2",
   custom: {
     webpack: {
-      webpackConfig: './webpack.config.js',
-      includeModules: true
-    }
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
   },
   // Add the serverless-webpack plugin
-  plugins: ['serverless-webpack'],
+  plugins: ["serverless-webpack"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs12.x',
+    name: "aws",
+    profile: "learning",
+    region: "ap-southeast-1",
+    runtime: "nodejs12.x",
     apiGateway: {
       minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+    },
+  },
+  resources: {
+    Resources: {
+      SiteNoteTable: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "SiteNoteTable-${self:provider.stage}",
+          BillingMode: "PAY_PER_REQUEST",
+          AttributeDefinitions: [{ AttributeName: "id", AttributeType: "S" }],
+          KeySchema: [{ AttributeName: "id", KeyType: "HASH" }],
+        },
+      },
     },
   },
   functions: {
     hello: {
-      handler: 'handler.hello',
+      handler: "handler.hello",
       events: [
         {
           http: {
-            method: 'get',
-            path: 'hello',
-          }
-        }
-      ]
-    }
-  }
-}
+            method: "get",
+            path: "hello",
+          },
+        },
+      ],
+    },
+  },
+};
 
 module.exports = serverlessConfiguration;
