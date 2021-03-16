ae3dc24f4bd7d0b17a7e6c9a0f43030e6727710e
Duy Phan <duyphan@lecle.co.kr>
2021-03-16 16:23:49 +0700

    style: text editor

--- packages/extension/scripts/HTMLElement.js
+++ packages/extension/scripts/HTMLElement.js
@@ -1,5 +1,15 @@
 var htmlElement = `
   <div class="sn sn-layout">
     <button class="sn sn-toggle-button">+</button>
+    <div class="sn sn-text-editor-container">
+      <div class="sn sn-text-editor-toolbar">
+        <a href="javascript:void(0)" class="sn sn-text-editor-handler sn-bold-handler"><strong>B</strong></a>
+        <a href="javascript:void(0)" class="sn sn-text-editor-handler sn-italic-handler"><i>I</i></a>
+        <a href="javascript:void(0)" class="sn sn-text-editor-handler sn-underline-handler"><u>U</u></a>
+
+        <button type="button" class="sn sn-close-text-editor-button"></button>
+      </div>
+      <div class="sn-text-editor" contenteditable="true"></div>
+    </div>
   </div>
 `;
